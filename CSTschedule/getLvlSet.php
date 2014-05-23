d651edba8a013b40c202b9b327269791d26a16b5
Denis Turitsa <dturitsa@yahoo.ca>
2014-05-23 01:13:32 -0700

    get schedule for User's Set

--- CSTschedule/getLvlSet.php
+++ CSTschedule/getLvlSet.php
@@ -1,4 +1,16 @@
 <?php
+	include 'functions.php';
+	require_once('config.php');
+	//$admin = 0;
+	//echo "testting yo";
 	session_start();
-	echo $_SESSION["SESS_levelAndSet"];
+
+	// Connect to server and select database.
+	mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)or die("cannot connect, error: ".mysql_error());
+	mysql_select_db(DB_DATABASE)or die("cannot select DB, error: ".mysql_error());
+	$tbl_name="accounts"; // Table name
+	if(isLoggedIn()) {
+		echo $_SESSION["SESS_levelAndSet"];
+	}
+	
 ?>
